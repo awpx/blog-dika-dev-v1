e2c879c1a7bef42683d116690e5ea07571733a97
awp <awpxwork@gmail.com>
2020-08-07 13:32:00 +0700

    fetch postlist using reducer

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,33 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Container, Row, Col } from 'react-bootstrap'
 import { PostCard } from './PostCard'
+import { listPosts } from '../actions/postActions'
 
 
 export const PostList = () => {
-  const [posts, setPosts] = useState([])
+  const dispatch = useDispatch()
+
+  const postList = useSelector(state => state.postList)
+  const { loading, error, posts } = postList
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const { data } = await axios.get('/api/v1/posts')
-
-      setPosts(data)
-    }
-
-    fetchPosts()
-  }, [])
+    dispatch(listPosts())
+    
+  }, [dispatch])
 
   return (
     <>
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col lg={10}>
-          {posts.map((post) => (
-              <PostCard post={post} key={Number(post._id)} />
-          ))}
-          </Col>
-        </Row>
-      </Container>
+    {loading 
+      ? (<h2>Loading...</h2>)
+      : error
+      ? (<h3>{error}</h3>)
+      : (
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col lg={10}>
+            {posts.map((post) => (
+                <PostCard post={post} key={Number(post._id)} />
+            ))}
+            </Col>
+          </Row>
+        </Container>
+      )}
+      
     </>
   )
 }
--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import {
+  postListReducer,
+} from './reducers/postReducers'
 
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+  postList: postListReducer,
+})
 
 const initialState = {}
 
