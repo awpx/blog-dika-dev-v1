c664e71bb216734348931c05a2502cc809c20f5a
awp <awpxwork@gmail.com>
2020-08-13 14:24:00 +0700

    add auth protect middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -23,3 +23,21 @@ export const authUser = asyncHandler( async (req, res) => {
     throw new Error('Invalid Email or Password')
   }
 })
+
+//@desc       get user profile
+//@route      GET /api/v1/users/profile
+//@access     private
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if(user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
